593803a44a212aefb3c9b9e8677d185a12498a13
TangGuoC <2958242107@qq.com>
2024-01-07 20:46:15 +0800

    [新增]完善通讯录中查看指定的数据API

--- project/addressBook.c
+++ project/addressBook.c
@@ -9,6 +9,9 @@ enum STATUS_CODE
     ON_SUCCESS,
     MALLOC_ERROR,
     NULL_PTR,
+    INVALID_ACCESS,
+    NOT_FIND,
+    EDIT_FAIL,
 }; 
 
 /* 判断传入指针是否正确 */
@@ -61,11 +64,113 @@ int addressBookDelAppointVal(addressBook ** pTxl)
 
 }
 
-/* 通讯录中查询指定的数据 */
-int addressBookGetAppointVal(addressBook ** pTxl)
-{
 
+/* 找到指定name所在的位置 */   
+static int addressBookGetAppointNamePos(addressBook *pTxl, char *name, int *pPos)
+{
+    int pos = 0;
+    /* 构建遍历结点 */
+    contactNode *travelNode = pTxl->head;
+
+    int cmp = 0;
+    /* 从头遍历找到指定的name */
+    while(travelNode)
+    {
+        cmp = strcmp(name, travelNode->name);
+        if(cmp == 0)
+        {
+            /* 解引用 */
+            *pPos = pos;
+            return pos;
+        }
+        travelNode = travelNode->next;
+        pos++;
+    }
+    /* 解引用 */  
+    *pPos = -1;
+    return NOT_FIND;
 }
+
+/* 找到指定phone所在的位置 */
+static int addressBookGetAppointPhonePos(addressBook *pTxl, char *phone, int *pPos)
+{
+    int pos = 0;
+    /* 构建遍历结点 */
+    contactNode *travelNode = pTxl->head;
+
+    int cmp = 0;
+    /* 从头遍历找到指定的name */
+    while(travelNode)
+    {
+        cmp = strcmp(phone, travelNode->phone);
+        if(cmp == 0)
+        {
+            /* 解引用 */
+            *pPos = pos;
+            return pos;
+        }
+        travelNode = travelNode->next;
+        pos++;
+    }
+    /* 解引用 */  
+    *pPos = -1;
+    return NOT_FIND;
+}
+
+/* 找到指定address所在的位置 */
+static int addressBookGetAppointAddressPos(addressBook *pTxl, char *address, int *pPos)
+{
+    int pos = 0;
+    /* 构建遍历结点 */
+    contactNode *travelNode = pTxl->head;
+
+    int cmp = 0;
+    /* 从头遍历找到指定的name */
+    while(travelNode)
+    {
+        cmp = strcmp(address, travelNode->address);
+        if(cmp == 0)
+        {
+            /* 解引用 */
+            *pPos = pos;
+            return pos;
+        }
+        travelNode = travelNode->next;
+        pos++;
+    }
+    /* 解引用 */  
+    *pPos = -1;
+    return NOT_FIND;
+}
+
+/* 通讯录中查询指定的数据 */
+int addressBookGetAppointVal(addressBook * pTxl, char *name, char *phone, char *address)
+{
+    int pos = 0;
+    if(name)
+    {
+        contactNode *travelNode = pTxl->head;
+        addressBookGetAppointNamePos(pTxl, name, pos);
+    }
+    else if(phone)
+    {
+        contactNode *travelNode = pTxl->head;
+        addressBookGetAppointNamePos(pTxl, phone, pos);
+    }
+    else if(address)
+    {
+        contactNode *travelNode = pTxl->head;
+        addressBookGetAppointNamePos(pTxl, address, pos);
+    }
+    else
+    {
+        printf("没有找到\n");
+        return EDIT_FAIL;
+    }
+    return ON_SUCCESS;
+}
+
+
 
 /* 通讯录中修改指定的指定数据 */
 int addressBookModify(addressBook ** pTxl)
--- project/addressBook.h
+++ project/addressBook.h
@@ -34,8 +34,9 @@ int addressBookInsert(addressBook ** pTxl);
 /* 通讯录中删除数据 */
 int addressBookDelAppointVal(addressBook ** pTxl);
 
+
 /* 通讯录中查询指定的数据 */
-int addressBookGetAppointVal(addressBook ** pTxl);
+int addressBookGetAppointVal(addressBook * pTxl, char *name, char *phone, char *address);
 
 /* 通讯录中修改指定的指定数据 */
 int addressBookModify(addressBook ** pTxl);
